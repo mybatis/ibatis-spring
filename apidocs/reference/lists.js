eed77c2ef56fcccba44a835ad6be0e2e1bead2a5
Jeremy Landis <jeremylandis@hotmail.com>
2015-11-07 20:53:55 -0500

    wagon-git commit

---
+++ apidocs/reference/lists.js
@@ -0,0 +1,5 @@
+var DATA = [
+      { id:0, label:"org.ibatis.spring", link:"org/ibatis/spring/package-summary.html", type:"package" },
+      { id:1, label:"org.ibatis.spring.Ibatis2Support", link:"org/ibatis/spring/Ibatis2Support.html", type:"class" }
+
+    ];
