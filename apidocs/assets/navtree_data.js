2d13313006ae2d4a39667cdd8cef7c125ea0a9a1
hazendaz <jeremylandis@hotmail.com>
2024-02-10 22:37:01 +0000

    Deploying to gh-pages from @ mybatis/ibatis-spring@207c90eea20e536b325e55adfa9eb28d06f7bc53 🚀

--- apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.ibatis.spring", "org/ibatis/spring/package-summary.html", [ [ "Classes", null, [ [ "Ibatis2Support", "org/ibatis/spring/Ibatis2Support.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- apidocs/reference/lists.js
+++
@@ -1,5 +0,0 @@
-var DATA = [
-      { id:0, label:"org.ibatis.spring", link:"org/ibatis/spring/package-summary.html", type:"package" },
-      { id:1, label:"org.ibatis.spring.Ibatis2Support", link:"org/ibatis/spring/Ibatis2Support.html", type:"class" }
-
-    ];
